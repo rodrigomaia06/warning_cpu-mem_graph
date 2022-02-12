13ea21cae1534156846283537365089809302680
VladimirBromley0750 <91988865+VladimirBromley0750@users.noreply.github.com>
2022-02-12 00:35:14 +0000

    Update sendmailwarningmem.py

--- versions/1.0/sendmailwarningmem.py
+++ versions/1.0/sendmailwarningmem.py
@@ -6,7 +6,7 @@ password = base64.b64decode(password_base64).decode("utf-8")
 rec_email = "rodrigo.m.t.maia@gmail.com"
 
 subject = "WARNING!"
-body = "Mem_server >= ###percentage_warning### %"
+body = "Mem_server >= ###ram_percentage_warning### %"
 msg = f"Subject: {subject}\n\n{body}"
 
 server = smtplib.SMTP("smtp.gmail.com", 587)
