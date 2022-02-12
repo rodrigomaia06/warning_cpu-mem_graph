9934c7848dbbd21242247c447711071e66197665
VladimirBromley0750 <91988865+VladimirBromley0750@users.noreply.github.com>
2022-02-12 01:09:06 +0000

    Update dependencies.sh

--- versions/1.0/dependencies.sh
+++ versions/1.0/dependencies.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sudo 
+sudo apt install bc &> /dev/null
+sudo apt install python3 &> /dev/null
+sudo apt install wget &> /dev/null
+sudo apt install pip &> /dev/null
+pip install matplotlib &> /dev/null
