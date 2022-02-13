a9b4150fb06b7da821fc18aae9f8d2d197af9bee
VladimirBromley0750 <91988865+VladimirBromley0750@users.noreply.github.com>
2022-02-13 22:07:28 +0000

    Update graphmath.py

--- versions/1.0/graph-data/graphmath.py
+++ versions/1.0/graph-data/graphmath.py
@@ -11,9 +11,9 @@ for line in open('graphdata.txt', 'r'):
     y.append(int(lines[1]))
     z.append(int(lines[2]))
 
-plt.axhline(y=###cpu_percentage_warning###, color='g', linestyle='dashed')
+plt.axhline(y=###cpu_percentage_warning###, color='g', linestyle='dashed', linewidth=1, label='CPU Warning')
 plt.plot(x, y, 'g', label='CPU Usage')
-plt.axhline(y=###ram_percentage_warning###, color='r', linestyle='dashed')
+plt.axhline(y=###ram_percentage_warning###, color='r', linestyle='dashed', linewidth=1, label='MEM Warning')
 plt.plot(x, z, 'r', label='MEM Usage')
 plt.xticks(np.arange(0, len(x), step=math.floor(len(x)/8)))
 plt.legend()
