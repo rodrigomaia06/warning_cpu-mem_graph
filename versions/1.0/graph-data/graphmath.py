51c6ffcbf5ca46a41e9c65c44bf4c42d5c7e807e
VladimirBromley0750 <91988865+VladimirBromley0750@users.noreply.github.com>
2022-02-13 21:54:18 +0000

    Update graphmath.py

--- versions/1.0/graph-data/graphmath.py
+++ versions/1.0/graph-data/graphmath.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
-  
+import numpy as np
+import math
+
 x = []
 y = []
 z = []
@@ -8,10 +10,12 @@ for line in open('graphdata.txt', 'r'):
     x.append(lines[0])
     y.append(int(lines[1]))
     z.append(int(lines[2]))
-      
+
+plt.axhline(y=###cpu_percentage_warning###, color='g', linestyle='dashed')
 plt.plot(x, y, 'g', label='CPU Usage')
+plt.axhline(y=###ram_percentage_warning###, color='r', linestyle='dashed')
 plt.plot(x, z, 'r', label='MEM Usage')
+plt.xticks(np.arange(0, len(x), step=math.floor(len(x)/8)))
 plt.legend()
 plt.savefig('plot.png', dpi=300, bbox_inches='tight')
-  
 plt.show()
