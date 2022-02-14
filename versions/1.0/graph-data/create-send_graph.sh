13298e87aaa731dc2ee62b1f44ec8cb065a1d179
Rodrigo Maia <91988865+rodrigomaia06@users.noreply.github.com>
2022-02-14 21:49:22 +0000

    Update create-send_graph.sh

--- versions/1.0/graph-data/create-send_graph.sh
+++ versions/1.0/graph-data/create-send_graph.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 PC_USER=`whoami`
-PREVIOUSDATE=`date +%F`
-NEXTDATE=`date -d '+1 day' +%F`
+DATE=`date +%F`
 cd /home/$PC_USER/.scriptmail-dependencies/graph-data
 python3 graphmath.py
-mv plot.png graph_$PREVIOUSDATE.png
-python3 sendemailgraph.py
+mv plot.png graph_$DATE.png
+python3 sendemailgraph.py $DATE
 cat /dev/null > graphdata.txt
-cp -rf sendemailgraph_bkp.py sendemailgraph.py
-sed -i "s,###lastrunned###,""$NEXTDATE"',' sendemailgraph.py
 rm graph_$PREVIOUSDATE.png
